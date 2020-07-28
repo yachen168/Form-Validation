ae31d841d52a4f773af2d3ac28182a326cf23eed
yachen <io260188@gmail.com>
2020-07-29 01:28:12 +0800

    refactor: Header component

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,23 @@
 <template>
   <div id="app">
-		<header></header>
+		<Header></Header>
 		<keep-alive>
       <router-view/>
 		</keep-alive>
   </div>
 </template>
+
+<script>
+import Header from '@/components/Header'
+
+export default {
+	components:{
+		Header
+	}
+}
+</script>
 	
 <style lang="scss">
-header {
-	height: 5px;
-	max-width: 460px;
-	margin: 0 auto 50px;
-	background-color: $blue-200;
-}
+// #app
 
-#nav {
-	padding: 30px;
-	text-align: center;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+<header></header>
+</template>
+
+<script>
+	export default {
+		
+	}
+</script>
+
+<style lang="scss" scoped>
+header {
+	height: 5px;
+	max-width: 460px;
+	margin: 0 auto 50px;
+	background-color: $blue-200;
+}
+</style>
