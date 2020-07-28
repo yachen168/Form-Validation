19fbc46205b4043764885130ebc74c8e2711747a
yachen <io260188@gmail.com>
2020-07-28 22:52:10 +0800

    build: vuex

--- src/main.js
+++ src/main.js
@@ -1,17 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
+import store from './store'
 import router from './router'
 
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { faImages, faSmile, faExclamationTriangle, faTrashAlt } from "@fortawesome/free-solid-svg-icons";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { far } from "@fortawesome/free-regular-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
-library.add(faImages, faSmile, faExclamationTriangle, faTrashAlt);
+library.add(fas, far);
+
 Vue.component("font-awesome-icon", FontAwesomeIcon);
 
 Vue.config.productionTip = false
 
 new Vue({
     router,
+    store,
     render: h => h(App)
 }).$mount('#app')
