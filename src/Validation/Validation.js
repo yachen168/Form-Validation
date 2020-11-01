1d56cf5059ff089e9dc09f293f2dd837c6ee1e83
yachen <io260188@gmail.com>
2020-11-01 23:26:40 +0800

    style: format code

--- src/Validation/Validation.js
+++ src/Validation/Validation.js
@@ -1,14 +1,14 @@
 export default {
-    creatAccount: {
-        checkAccount: /^[A-z0-9]+@[A-z]+\.com{1}$/, // 英文或數字@英文.com
-        checkPassword: /^[\d|A-z]{8,}$/ // 至少 8 個英文或數字
-    },
-    generalInformation: {
-        checkPhoneNumber: /^09[\d]{8}$/, // 09開頭10個數字
-        checkAddress: /[\u4e00-\u9fa5]/, // 至少一個中文字
-    },
-    paymentMethod: {
-        checkCardNumber: /^(\d{4}\s){3}\d{4}$/, // 16個數字(4個一組)
-        checkBankName: /^[A-z]{2,}$/ // 至少兩個英文字
-    }
-}
+  creatAccount: {
+    checkAccount: /^[A-z\d]+@[A-z]+\.com{1}$/, // 英文或數字@英文.com
+    checkPassword: /^[\d|A-z]{8,}$/, // 至少 8 個英文或數字
+  },
+  generalInformation: {
+    checkPhoneNumber: /^09[\d]{8}$/, // 09開頭10個數字
+    checkAddress: /[\u4e00-\u9fa5]/, // 至少一個中文字
+  },
+  paymentMethod: {
+    checkCardNumber: /^(\d{4}\s){3}\d{4}$/, // 16個數字(4個一組)
+    checkBankName: /^[A-z]{2,}$/, // 至少兩個英文字
+  },
+};
--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
-import Vue from 'vue'
-import App from './App.vue'
-import store from './store'
-import router from './router'
+import Vue from "vue";
+import App from "./App.vue";
+import store from "./store";
+import router from "./router";
 
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fas } from "@fortawesome/free-solid-svg-icons";
@@ -12,10 +12,10 @@ library.add(fas, far);
 
 Vue.component("font-awesome-icon", FontAwesomeIcon);
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
-    router,
-    store,
-    render: h => h(App)
-}).$mount('#app')
+  router,
+  store,
+  render: (h) => h(App),
+}).$mount("#app");
